c3f606b8c8f377a466e7d71cf68b92d97e5c46bd
Marcos Silva <marcos.silva79@gmail.com>
2020-01-15 09:28:33 -0500

    Configured mongodb connection with Atlas service

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //Database ATLAS connection 
-//const uri = process.env.ATLAS_URI; //URI from mongoDB Atlas page
-//mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex:true});
+const uri = process.env.ATLAS_URI; //URI from mongoDB Atlas page
+
+mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex:true});
 
 //Database local connection
-mongoose.connect('mongodb://localhost/jobtrackdb',{useNewUrlParser: true, useCreateIndex:true});
+//mongoose.connect('mongodb://localhost/jobtrackdb',{useNewUrlParser: true, useCreateIndex:true});
 
 
 
